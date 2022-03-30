24a4554f1a810accfe4a5c324b61b3d23235eb04
vinayaka-pu <102621844+vinayaka-pu@users.noreply.github.com>
2022-03-30 22:35:53 +0530

    Add files to src folder

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./css/header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <p>Revolve Solution</p>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Search</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
